4d03c771605d1a690f42fa849327b282026a55ef
Moiseev Konstantin <const.moiseew2012@yandex.ru>
2026-03-07 17:44:29 +0300

    Парсинг командной строки с возможностью вводить входной и выходной файлы относительно рабочей директории pwd

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include "dlist.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
+    std::string input_file, output_file;
+    for (int i = 0; i < argc; i++) {  
+        std::string arg = argv[i];  
+        
+        if (arg == "-in" && i + 1 < argc) {
+            input_file = argv[i + 1];
+            i++;  
+        }
+        if (arg == "-out" && i + 1 < argc) {
+            output_file = argv[i + 1];
+            i++;  
+        }
+    }
+    
     DList* list = new DList();
-    //list->Set_file_path("/home/const/repos/double_linked_list/dllist-serializer/res/inlet.in");
-    //list->Set_file_path("/home/const/repos/double_linked_list/dllist-serializer/res/outlet.out");
+    if(!input_file.empty()){
+        list->Set_file_in_path(input_file);
+    }
+    if(!output_file.empty()){
+        list->Set_file_out_path(output_file);
+    }
     list->ReadFileIn();
     list->Serialize();
     
